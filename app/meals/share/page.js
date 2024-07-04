4bfb5f1839ea45bd9960d0035f7e198a1de92b11
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-07-04 08:00:30 +0200

    add form validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,10 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={classes.header}>
@@ -16,8 +18,8 @@ export default function ShareMealPage() {
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-{              <input type="text" id="name" name="name" required />
-}            </p>
+              <input type="text" id="name" name="name" required />
+            </p>
             <p>
               <label htmlFor="email">Your email</label>
               <input type="email" id="email" name="email" required />
@@ -42,7 +44,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
